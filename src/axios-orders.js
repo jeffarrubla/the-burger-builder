b42d5a1fc24d5abc0a162bec5202d7973a463c98
Jefferson <jeffarrubla@gmail.com>
2020-02-13 20:08:25 +0000

    Handling errors

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://react-my-burger-fe382.firebaseio.com/'
+	baseURL: /* put your url here */
 });
 
 export default instance;
