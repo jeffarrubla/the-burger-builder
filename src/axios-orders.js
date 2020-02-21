f3a8b21e3394f355ea5228ecdba01c367ee8b6eb
Jefferson <jeffarrubla@gmail.com>
2020-02-21 18:09:31 +0000

    Showing error messages

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: /*Your url here*/
+	baseURL:  /*Your url here*/
 });
 
 export default instance;
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,9 +5,11 @@ import classes from './Input.module.css';
 const input = (props) => {
 	let inputElement = null;
 	const inputClasses = [classes.InputElement];
+	let validationError = null;
 
 	if(props.invalid && props.shouldValidate && props.touched) {
 		inputClasses.push(classes.Invalid)
+		validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
 	}
 
 	switch(props.elementType) {
@@ -47,6 +49,7 @@ const input = (props) => {
 		<div  className={classes.Input}>
 			<label className={classes.Label}>{props.label}</label>
 			{inputElement}
+			{validationError}
 		</div>
 	);
 };
