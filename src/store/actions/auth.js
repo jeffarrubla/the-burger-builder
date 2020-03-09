8ac6a48ced25a022a80dc7f5c221291840b653fb
Jefferson <jeffarrubla@gmail.com>
2020-03-09 20:58:39 +0000

    Adding sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,15 +22,19 @@ export const authFail = (error) => {
 	};
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
 	return dispatch => {
 		dispatch(authStart());
 		const authData = {
 			email: email,
 			password: password,
 			returnSecureToken: true
+		};
+		let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]';
+		if( !isSignup ){
+			url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]';
 		}
-		axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]', authData)
+		axios.post(url, authData)
 			.then(response => {
 				console.log(response);
 				dispatch(authSuccess(response.date));
